2fe91fcaaf9f0756e191a777e3469b7b1ab032ca
Azka Azmi <azmia@teach.cs.utoronto.ca>
2025-03-06 11:30:46 -0500

    fixed slide issue

--- js/emotion-graph.js
+++ js/emotion-graph.js
@@ -23,8 +23,11 @@ class EmotionGraph {
 
 		vis.margin = {top: 10, right: 10, bottom: 10, left: 10};
 
-		vis.width = document.getElementById(vis.parentElement).getBoundingClientRect().width - vis.margin.left - vis.margin.right;
-		vis.height = document.getElementById(vis.parentElement).getBoundingClientRect().height - vis.margin.top - vis.margin.bottom;
+		// vis.width = document.getElementById(vis.parentElement).getBoundingClientRect().width - vis.margin.left - vis.margin.right;
+		// vis.height = document.getElementById(vis.parentElement).getBoundingClientRect().height - vis.margin.top - vis.margin.bottom;
+
+        vis.width = 1000;
+		vis.height = 700;
 
         // SVG drawing area
         vis.svg = d3.select("#" + vis.parentElement).append("svg")
